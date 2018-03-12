e7d3561a3a08d8229633b563f399b736eb27b889
Carlos del Rio <cfdelrio@gmail.com>
2018-03-12 09:43:45 -0300

    implement get product by id in server side

--- server/server.js
+++ server/server.js
@@ -1,28 +1,56 @@
 var express = require('express');
 var app = express();
+var db = [
+    {"author": {
+            "name": "Joe",
+            "lastName": "Beck"
+            },   
+    "categories": ["cellphone","technology"],
+    "items": [{ "id": "N98",
+        "title": "Cellphone",      
+        "price": { "currency": "Pesos",
+                    "amount": 200,
+                    "decimals": 0
+                },       
+        "picture": "",
+        "condition": "New", 
+        "free_shipping": false
+        }]
+    },
+    {"author": {
+        "name": "Joe",
+        "lastName": "Beck"
+        },   
+    "categories": ["cellphone","technology"],
+    "items": [{ "id": "N989",
+        "title": "Cellphone",      
+        "price": { "currency": "Pesos",
+                    "amount": 200,
+                    "decimals": 10
+                },       
+        "picture": "",
+        "condition": "New", 
+        "free_shipping": false
+        }]
+    }
+];
+
 
 app.get('/', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(
-        {   "author": 
-            {
-            "name": "Joe",
-             "lastname": "Beck"
-            },   
-            "categories": ["cellphone","technology"],
-            "items": [{ "id": "N98",
-                        "title": "Cellphone",      
-                        "price": { "currency": "Pesos",
-                         "amount": 200,
-                         "decimals": 0
-                        },       
-                        "picture": "",
-                        "condition": "New", 
-                        "free_shipping": false
-                    }]
-        }  
-    ));
+    res.status(200).send(db);
 })
+
+app.get('/api/items/:id', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    var productID = req.params.id;
+    const response = db.filter(function(list) {
+        // TODO refactor pls ASAP
+        return list.items[0].id === productID;
+    });
+    res.status(200).send(response);
+})
+
 
 var server = app.listen(8081, function () {
    var host = server.address().address
