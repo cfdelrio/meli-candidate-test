bc7bd042c893af1e38c72a55a87e9fb9bfa2ece7
Carlos del Rio <cfdelrio@gmail.com>
2018-03-12 00:58:13 -0300

    rest api working in progress

--- src/js/components/presentational/products-list.js
+++ src/js/components/presentational/products-list.js
@@ -2,9 +2,16 @@ import React from 'react';
 import qs from "query-string";
 
 export default class ProductsList extends React.Component {
+    componentWillMount() {
+        fetch('http://localhost:8081/', {mode: 'no-cors',dataType: 'json'})
+          .then((response) => {
+            this.setState({ items: response });
+          })
+    }
+
     render() {
         const searchTerm = qs.parse(this.props.location.search);
-        
+        console.log(this.state);
         return (
             <div>
                 <h3>product list search:{searchTerm.search}</h3>
